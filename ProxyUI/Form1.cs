69bb9456f622131265d40a816758712071cb11f1
mrcyclo <vipboysanhdieu@gmail.com>
2024-09-07 16:39:13 +0700

    Update testing form

--- ProxyUI/Form1.cs
+++ ProxyUI/Form1.cs
@@ -2,6 +2,7 @@ using CaptureProxy;
 using CaptureProxy.MyEventArgs;
 using System.Net;
 using System.Text;
+using System.Windows.Forms;
 
 namespace ProxyUI
 {
@@ -16,30 +17,34 @@ namespace ProxyUI
             button1.Enabled = true;
             button2.Enabled = false;
 
-            CaptureProxy.Events.Logger = (string message) =>
+            proxy.Events.LogReceived += Events_LogReceived;
+            proxy.Events.BeforeTunnelEstablish += Events_BeforeTunnelEstablish;
+            proxy.Events.BeforeRequest += Events_BeforeRequest;
+            proxy.Events.BeforeHeaderResponse += Events_BeforeHeaderResponse;
+            proxy.Events.BeforeBodyResponse += Events_BeforeBodyResponse;
+        }
+
+        private void Events_LogReceived(object? sender, string message)
+        {
+            message = $"[{DateTime.Now}] {message}\r\n";
+
+            try
             {
-                message = $"[{DateTime.Now}] {message}\r\n";
                 Invoke(new Action(() =>
                 {
                     richTextBox1.AppendText(message);
                     richTextBox1.ScrollToCaret();
                 }));
+            }
+            catch { }
 
-                File.AppendAllText("logs.txt", message);
-            };
-
-            CaptureProxy.Events.BeforeTunnelEstablish += Events_BeforeTunnelEstablish;
-            CaptureProxy.Events.BeforeRequest += Events_BeforeRequest;
-            CaptureProxy.Events.BeforeHeaderResponse += Events_BeforeHeaderResponse;
-            CaptureProxy.Events.BeforeBodyResponse += Events_BeforeBodyResponse;
+            //File.AppendAllText("logs.txt", message);
         }
 
         private void Events_BeforeTunnelEstablish(object? sender, BeforeTunnelEstablishEventArgs e)
         {
             //e.PacketCapture = true;
             e.UpstreamProxy = true;
-            e.Host = "localhost";
-            e.Port = 25344;
         }
 
         private void Events_BeforeRequest(object? sender, BeforeRequestEventArgs e)
@@ -49,12 +54,12 @@ namespace ProxyUI
 
         private void Events_BeforeHeaderResponse(object? sender, BeforeHeaderResponseEventArgs e)
         {
-            
+
         }
 
         private void Events_BeforeBodyResponse(object? sender, BeforeBodyResponseEventArgs e)
         {
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -72,11 +77,11 @@ namespace ProxyUI
             button2.Enabled = true;
         }
 
-        private async void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
             button2.Enabled = false;
 
-            await Task.Run(() => proxy.Stop());
+            proxy.Stop();
 
             bool successful = false;
             while (!successful)
@@ -89,12 +94,12 @@ namespace ProxyUI
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            button2_Click(null, null);
+            button2_Click(sender, e);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Text = "Session: " + Session.Collection.Count;
+            label1.Text = "Session: " + proxy.SessionCount;
         }
     }
 }
