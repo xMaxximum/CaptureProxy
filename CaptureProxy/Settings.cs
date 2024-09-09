b8734ffc2d5579952d278d913b3e0a65f8aa362b
Le Tuan Anh <anhlt2@vnext.vn>
2024-09-09 12:25:49 +0700

    Increase max chunkd size to 64kB

--- CaptureProxy/Settings.cs
+++ CaptureProxy/Settings.cs
@@ -5,6 +5,6 @@
         public int ReadTimeoutMs = 30000;
         public int MaxIncomingHeaderLine = 64 * 1024;
         public int StreamBufferSize = 64 * 1024;
-        public int MaxChunkSizeLine = 1024;
+        public int MaxChunkSizeLine = 64 * 1024;
     }
 }
